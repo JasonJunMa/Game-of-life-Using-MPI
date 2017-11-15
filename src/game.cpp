ee23dd1288656a8a926978c381422f2886729b1b
JasonJunMa <majun900313@gmail.com>
2017-11-15 09:50:53 -0500

    blabla

--- src/game.cpp
+++ src/game.cpp
@@ -3,18 +3,17 @@
 #include "cell.h"
 using namespace std;
 
+#define sizeOfBoard 1000
+
 int main(int argc, char *argv[])
 {
-    int size, rank;
-    Cell board[1000][1000];
-    MPI_Status Stat;
-    srand(time(NULL));
-
     /**
      * Initilized a board
      * Set random state for each cell
      * 0 for dead 1 for live
      */
+    Cell board[sizeOfBoard][sizeOfBoard];
+    srand(time(NULL));
     for (int i = 0; i < 1000; i++)
     {
         for (int j = 0; j < 1000; j++)
@@ -29,14 +28,25 @@ int main(int argc, char *argv[])
     /**
      * Init MPI
      */
+    int size, rank;
+    MPI_Status Stat;
     int rc = MPI_Init(&argc, &argv);
     if (rc != 0)
     {
         cout << "Error starting MPI." << endl;
         MPI_Abort(MPI_COMM_WORLD, rc);
     }
+    // Get the size of the processor
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    // Get the current processor rank
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0)
+    {
+    }
+
+    // Finish the MPI
+    MPI_Finalize();
 
     return 0;
 }
